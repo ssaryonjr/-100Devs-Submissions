56a3300604d76af98aad22ec18475ed8504154d2
Sam Saryon <ssaryonjr@gmail.com>
2022-04-27 16:13:41 -0700

    Calculator project

---
+++ OOP Calculator/script.js
@@ -0,0 +1,14 @@
+class Calculator {
+    constructor(prevText, currText){
+        
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const prevText = document.querySelectorAll('[data-previous-operand]')
+const currText = document.querySelectorAll('[data-current-operand]')
