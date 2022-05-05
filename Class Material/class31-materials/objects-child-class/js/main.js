ff4b604c48bd570068b36a326e6fba904f54484c
Sam Saryon <ssaryonjr@gmail.com>
2022-05-04 20:24:59 -0700

    Small changes

--- Class Material/class31-materials/objects-child-class/js/main.js
+++ Class Material/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,17 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name)
+        this.breed = breed
+    } 
+}
 
+let simba = new Dog('Simba', 'Sheperd')
