4ab8b25a1432260e948ad15631509c47d46a37a6
Sam Saryon <ssaryonjr@gmail.com>
2022-07-20 15:46:21 -0700

    review

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -7,7 +7,37 @@
 //If under over 30, tell them there is nothing left to look forward too
 
 
+
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+const checker = document.querySelector('h1');
+
+checker.addEventListener('click', ()=> {
+let age = Number(document.getElementById('danceDanceRevolution').value)
+
+let answer = document.querySelector('p');
+
+if (age < 16){
+    answer.innerText = 'Cant drive'
+    console.log('Cannot drive');
+} else if (age < 18) {
+    answer.innerText = `Can't hate from outside of the club because they can't even get in`
+    console.log(`Can't hate from outside of the club because they can't even get in`)
+} else if (age < 21){
+    answer.innerText = 'You cant drink'
+    console.log('You cant drink')
+} else if (age < 25){
+    answer.innerText = 'They cannot rent cars affordably'
+    console.log('They cannot rent cars affordably')
+} else if (age < 30){
+    answer.innerText = 'You cannot rent fancy cars affordably'
+    console.log('You cannot rent fancy cars affordably')
+} else if (age >= 30){
+    answer.innerText = 'theres nothing left to look forward to'
+    console.log('theres nothing left to look forward to')
+}
+
+})
--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -1,18 +1,37 @@
 //--- Easy
 //create a variable and assign it a number
+let num = 15
 
 //minus 10 from that number
 
+ num = num - 10;
+
 //print that number to the console
+
+console.log(num)
 
 //--- Medium
 //create a variable that holds a value from the input
+
 
 //add 25 to that number
 
 //alert that number
 
+
 //--- Hard
 //create a variable that holds the h1
 
+const title = document.querySelector('h1')
+
+
 //add an event listener to that element that console logs the sum of the two previous variables
+
+title.addEventListener('click', ()=> {
+
+    let input = Number(document.querySelector('#danceDanceRevolution').value)
+
+    input += 25
+
+    console.log(input + num)
+})
