3513d142c2adf8d7262c12afb261cc51f3c4f61c
Sam Saryon <ssaryonjr@gmail.com>
2022-07-21 00:08:43 -0700

    more hw done

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,19 @@
 //Write your pseduo code first! 
+//celcius to farhneight
+
+//get the value of celcious
+//take that input and multiplyit by 1.8 and add 32;
+//return output
+
+function converter(){
+    let temp = Number(document.querySelector('input').value)
+
+    temp = (temp * 1.8)+ 32
+    
+    document.querySelector('h2').innerText = temp
+}
+
+
+const click = document.querySelector('h1')
+
+click.addEventListener('click', converter)
