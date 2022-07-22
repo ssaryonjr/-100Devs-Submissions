7b0acb70d533dfd6273d6e9e2c5c5cda4d702889
Sam Saryon <ssaryonjr@gmail.com>
2022-07-22 14:06:37 -0700

    updating coinflip

--- Homework Assignments/coinflip/server.js
+++ Homework Assignments/coinflip/server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     fs.readFile('index.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
-      res.end();
+      res.end(); 
     });
   }
   else if (page == '/otherpage') {
